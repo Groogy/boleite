9490ab2475a95b681cd82f0c7d5da3547251503e
Henrik Valter Vogelius Hansson <groogy@groogy.se>
2017-07-30 00:50:51 +0200

    Fixed compile error and added white and black color

--- src/boleite/math/color.cr
+++ src/boleite/math/color.cr
@@ -1,15 +1,23 @@
 module Boleite
   module Color
     def self.red
-      Colorf.new(1.0, 0.0, 0.0, 1.0)
+      Colorf.new 1.0_f32, 0.0_f32, 0.0_f32, 1.0_f32
     end
 
     def self.green
-      Colorf.new(0.0, 1.0, 0.0, 1.0)
+      Colorf.new 0.0_f32, 1.0_f32, 0.0_f32, 1.0_f32
     end
 
     def self.blue
-      Colorf.new(0.0, 0.0, 1.0, 1.0)
+      Colorf.new 0.0_f32, 0.0_f32, 1.0_f32, 1.0_f32
+    end
+
+    def self.black
+      Colorf.new 0.0_f32, 0.0_f32, 0.0_f32, 1.0_f32
+    end
+
+    def self.white
+      Colorf.new 1.0_f32, 1.0_f32, 1.0_f32, 1.0_f32
     end
   end
 end
