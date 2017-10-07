91e3cb32fefdcb9da9efc410c3806366c9ae4cbd
Henrik Valter Vogelius Hansson <groogy@groogy.se>
2017-10-07 22:36:25 +0200

    Added so 3d camera can rotate

--- src/boleite/graphics/camera_3d.cr
+++ src/boleite/graphics/camera_3d.cr
@@ -1,6 +1,7 @@
 class Boleite::Camera3D < Boleite::Camera  
   @update_transform = true
   @pos = Vector3f.zero
+  @rot = Vector3f.zero
   
   def initialize(@fov : Float32, @width : Float32, @height : Float32, @near : Float32, @far : Float32)
     aspect = width / height
@@ -16,12 +17,29 @@ class Boleite::Camera3D < Boleite::Camera
     @update_transform = true
   end
 
+  def rotation
+    @rot
+  end
+
+  def rotation=(rot)
+    @rot = rot
+    @update_transform = true
+  end
+
   def move(offset)
     self.position = position + offset
   end
 
   def move(x, y, z)
     self.position = position + Vector3f.new(x, y, z)
+  end
+
+  def rotate(vec)
+    self.rotation = rotation + vec
+  end
+
+  def rotate(x, y, z)
+    self.rotation = rotation + Vector3f.new(x, y, z)
   end
 
   def transformation
@@ -35,5 +53,8 @@ class Boleite::Camera3D < Boleite::Camera
   def update_transformation
     @transform = Matrix44f32.identity
     @transform = Matrix.translate @transform, @pos.to_f32
+    @transform = Matrix.rotate_around_x @transform, @rot.x.to_f32
+    @transform = Matrix.rotate_around_y @transform, @rot.y.to_f32
+    @transform = Matrix.rotate_around_z @transform, @rot.z.to_f32
   end
 end
