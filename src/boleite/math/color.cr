10d3a0b198c131220e812e0af089221e54104e14
Henrik Valter Vogelius Hansson <groogy@groogy.se>
2017-10-14 21:50:07 +0200

    Refactored Color module to follow code standard

--- src/boleite/math/color.cr
+++ src/boleite/math/color.cr
@@ -1,22 +1,22 @@
 module Boleite::Color
   def self.red
-    Colorf.new 1.0_f32, 0.0_f32, 0.0_f32, 1.0_f32
+    Colorf.new 1f32, 0f32, 0f32, 1f32
   end
 
   def self.green
-    Colorf.new 0.0_f32, 1.0_f32, 0.0_f32, 1.0_f32
+    Colorf.new 0f32, 1f32, 0f32, 1f32
   end
 
   def self.blue
-    Colorf.new 0.0_f32, 0.0_f32, 1.0_f32, 1.0_f32
+    Colorf.new 0f32, 0f32, 1f32, 1f32
   end
 
   def self.black
-    Colorf.new 0.0_f32, 0.0_f32, 0.0_f32, 1.0_f32
+    Colorf.new 0f32, 0f32, 0f32, 1f32
   end
 
   def self.white
-    Colorf.new 1.0_f32, 1.0_f32, 1.0_f32, 1.0_f32
+    Colorf.new 1f32, 1f32, 1f32, 1f32
   end
 
   def self.yellow
