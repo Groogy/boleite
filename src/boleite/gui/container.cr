c6d6fdf4dbc2bf54994c31a5d4a835e36d7f7324
Henrik Valter Vogelius Hansson <groogy@groogy.se>
2017-11-04 15:52:10 +0100

    More work on containers

--- src/boleite/gui/container.cr
+++ src/boleite/gui/container.cr
@@ -3,14 +3,28 @@ class Boleite::GUI
   end
 
   abstract class Container < Widget
-    @children = [] of Widget
+    include CrystalClear
 
+    @children = [] of Widget
+    @min_size = Vector2f.new 0.0, 0.0
+
+    delegate :"[]", to: @children
+    getter min_size
+
+    requires child.parent.nil?
+    ensures @children.count child == 1
     def add(child)
       @children << child
+      child.parent = self
     end
 
+    requires child.parent == self
+    ensures @children.includes?(child) == false
+    ensures child.parent != self
     def remove(child)
-      @children.delete child
+      child = @children.delete child
+      child.parent = nil if child
+      child
     end
 
     def find(name)
@@ -19,10 +33,25 @@ class Boleite::GUI
       end
     end
 
-    def each_child
+    def num_widgets
+      @children.size
+    end
+
+    def clear
+      @children.each do |child|
+        child.parent = nil
+      end
+      @children.clear
+    end
+
+    def each_widget
       @children.each do |child|
         yield child
       end
+    end
+
+    def min_size=(@min_size)
+      self.size = @min_size if @min_size.x >= size.x || @min_size.y >= size.y
     end
   end
 end
