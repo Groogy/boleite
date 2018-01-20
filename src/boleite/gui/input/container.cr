10184bc425c36e3cf9350fb8567301365e21a940
Henrik Valter Vogelius Hansson <groogy@groogy.se>
2018-01-20 11:49:30 +0100

    We should just let containers pass all input

--- src/boleite/gui/input/container.cr
+++ src/boleite/gui/input/container.cr
@@ -1,20 +1,10 @@
 class Boleite::GUI
   class ContainerInputPass
-    @pos = Vector2f.zero
-
     def initialize(@widget : Container)
     end
 
     def interested?(event : InputEvent) : Bool
-      if event.is_a? MousePosEvent
-        @pos = event.pos
-      end
-      case event
-      when MousePosEvent, MouseButtonEvent, MouseScrollEvent
-        return @widget.absolute_allocation.contains? @pos
-      else
-        return true
-      end
+      true
     end
 
     def translate(event : InputEvent)
