fc4664ea068d560c789bbeba9122a4c46d3d0f81
Henrik Valter Vogelius Hansson <groogy@groogy.se>
2018-03-31 19:20:20 +0200

    Added support to move around 2d camera

--- src/boleite/graphics/camera_2d.cr
+++ src/boleite/graphics/camera_2d.cr
@@ -1,4 +1,6 @@
 class Boleite::Camera2D < Boleite::Camera
+  @pos = Vector2f.zero
+  
   property width, height, near, far
   
   def initialize(width : Float32, height : Float32, near : Float32, far : Float32)
@@ -9,7 +11,25 @@ class Boleite::Camera2D < Boleite::Camera
     @far = far
   end
 
+  def position
+    @pos
+  end
+
+  def position=(pos)
+    @update_transform = @update_transform || @pos != pos
+    @pos = pos
+  end
+
+  def move(offset)
+    self.position = position + offset
+  end
+
+  def move(x, y)
+    self.position = position + Vector2f.new(x, y)
+  end
+
   def update_transformation
     @transform = Matrix44f32.identity
+    @transform = Matrix.translate @transform, Vector3f32.new(@pos.x.to_f32, @pos.y.to_f32, 0f32)
   end
 end
