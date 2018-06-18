3b7f740f78beacf7bf0dfe0d2265fed16fae49e6
Henrik Valter Vogelius Hansson <groogy@groogy.se>
2018-06-18 22:39:43 +0200

    Added support to scale 2d camera

--- src/boleite/graphics/camera_2d.cr
+++ src/boleite/graphics/camera_2d.cr
@@ -1,7 +1,9 @@
 class Boleite::Camera2D < Boleite::Camera
   @pos = Vector2f.zero
+  @scale = 1.0
   
   property width, height, near, far
+  getter scale
   
   def initialize(width : Float32, height : Float32, near : Float32, far : Float32)
     super(Matrix.calculate_ortho_projection(0.0f32, width, 0.0f32, height, near, far))
@@ -20,6 +22,11 @@ class Boleite::Camera2D < Boleite::Camera
     @pos = pos
   end
 
+  def scale=(scale)
+    @update_transform = @update_transform || @scale != scale
+    @scale = scale
+  end
+
   def move(offset)
     self.position = position + offset
   end
@@ -29,7 +36,10 @@ class Boleite::Camera2D < Boleite::Camera
   end
 
   def update_transformation
+    p = @pos.to_f32
+    s = @scale.to_f32
     @transform = Matrix44f32.identity
-    @transform = Matrix.translate @transform, Vector3f32.new(@pos.x.to_f32, @pos.y.to_f32, 0f32)
+    @transform = Matrix.translate @transform, Vector3f32.new(p.x, p.y, 0f32)
+    @transform = Matrix.scale @transform, Vector4f32.new(s, s, s, 1f32)
   end
 end
