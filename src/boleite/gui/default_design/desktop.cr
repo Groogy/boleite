8f504bc7cd5aaca62352cfccb14c344f370eb274
Groogy <groogy@groogy.se>
2019-01-26 20:28:55 +0100

    Desktop children now adhere to it's position

--- src/boleite/gui/default_design/desktop.cr
+++ src/boleite/gui/default_design/desktop.cr
@@ -9,6 +9,9 @@ class Boleite::GUI
     class DesktopDesign < ContainerDesign
       def render(widget, transform, graphics)
         desktop = widget.as(Desktop)
+        pos2d = desktop.position.to_f32
+        pos3d = Boleite::Vector3f32.new pos2d.x, pos2d.y, 0f32
+        transform = Matrix.translate transform, pos3d
         render_children widget, transform, graphics
       end
     end
